dd2a02184b6f5a7ec7a4bb676f9fc9f5266ecb5c
Matthew Ng <matthewngchengen@Matthews-MacBook-Air.local>
2023-04-13 02:17:34 +0800

    simplied parameter choice in main.py

--- main.py
+++ main.py
@@ -639,7 +639,7 @@ def main(eqn_name, var_reduction, use_lstm=False, T=0.5, **kwargs):
 
 if __name__ == '__main__':
     main('BarrierOption',  var_reduction=False, K=[47, 52], B=[46, 53],
-         option_type=['call', 'put'], barrier_type=['up_and_out', 'down_and_out', 'up_and_in', 'down_and_in'])
+         option_type=['call'], barrier_type=['up_and_out'])
     main('LookbackOption', var_reduction=False, option_type=['call', 'put'])
 
 
